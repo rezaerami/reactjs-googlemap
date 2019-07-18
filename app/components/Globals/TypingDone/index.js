8d84e6c48ef98b51d81f8f24299de616ee1d62ca
Reza <Erami>
2019-07-18 20:20:43 +0430

    added document to typing done

--- app/components/Globals/TypingDone/index.js
+++ app/components/Globals/TypingDone/index.js
@@ -1,8 +1,38 @@
+/**
+ * @memberOf components.Globals
+ * @namespace components.Globals.TypingDone
+ */
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import omit from 'lodash/omit';
 
+/**
+ * class representing a component
+ * @memberOf components.Globals.TypingDone
+ * @class TypingDone
+ * @classdesc
+ * Typing done will trigger given function after user done typing
+ * @extends Component
+ * @example
+ * <TypingDone
+     onChangeValue={onChangeValue}
+     disabled={disabled}
+     onTypingDone={onTypingDone}
+     placeholder={placeholder}
+     value={value}
+     type={type}
+   />
+ */
 class TypingDone extends Component {
+  /**
+   * initializes TypingDone
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @constructs TypingDone
+   * @function constructor
+   * @description initializes default states and gives access to class through the handlers
+   * @param {object} props - props to extend
+   * @return void
+   */
   constructor(props) {
     super(props);
 
@@ -16,6 +46,13 @@ class TypingDone extends Component {
     this.handleClearTimer = this.handleClearTimer.bind(this);
   }
 
+  /**
+   * operations after mounting components
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function componentDidMount
+   * @description initializes given props in state after mounting component
+   * @return void
+   */
   componentDidMount() {
     const { value } = this.props;
     if (value && value !== '') {
@@ -23,6 +60,14 @@ class TypingDone extends Component {
     }
   }
 
+  /**
+   * operations after reseiving props
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function componentWillReceiveProps
+   * @description updates state using given props from parent
+   * @param {object}   nextProps    - updated props given from parent
+   * @return void
+   */
   componentWillReceiveProps(nextProps) {
     const { value: currentValue } = this.state;
     const { value } = nextProps;
@@ -33,6 +78,14 @@ class TypingDone extends Component {
     }
   }
 
+  /**
+   * sets a value in state and passes up to parent
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function handleValueChange
+   * @description sets given value to state of component and passes up to parent
+   * @param {object}   value    - value to put in state and pass up
+   * @return void
+   */
   handleValueChange(value) {
     const { onChangeValue } = this.props;
     this.setState(
@@ -46,6 +99,13 @@ class TypingDone extends Component {
     );
   }
 
+  /**
+   * sets an interval to tell the parent that typing is done
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function handleSetTimer
+   * @description sets an interval to tell the parent that typing is done
+   * @return void
+   */
   handleSetTimer() {
     const { delay, onTypingDone } = this.props;
     if (this.timer) {
@@ -57,10 +117,24 @@ class TypingDone extends Component {
     }, delay);
   }
 
+  /**
+   * clears the interval
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function handleSetTimer
+   * @descriptionclears the interval to avoid calling typing done again
+   * @return void
+   */
   handleClearTimer() {
     clearInterval(this.timer);
   }
 
+  /**
+   * renders TypingDone component
+   * @memberOf components.Globals.TypingDone.TypingDone
+   * @function render
+   * @description renders an input witch call given function when user done typing
+   * @return {jsx} - jsx component to show
+   */
   render() {
     const { value } = this.state;
     const rest = omit(this.props, ['onTypingDone', 'onChangeValue', 'delay']);
@@ -75,11 +149,27 @@ class TypingDone extends Component {
     );
   }
 }
+/**
+ * @memberOf components.Globals.TypingDone.TypingDone
+ * @name propTypes
+ * @type {object}
+ * @description defines prop types of TypingDone
+ * @property {boolean}     [disabled]             - defines that input is disabled
+ * @property {boolean}     [readOnly]             - defines that input is readonly
+ * @property {string}      [value]                - value of input
+ * @property {string}      [defaultValue]         - default value to initialize input
+ * @property {string}      [placeholder]          - placeholder to show when input is empty
+ * @property {string}      [type]                 - type of the input
+ * @property {number}      [delay]                - delay after user done typing
+ * @property {function}    onChangeValue          - function to change value
+ * @property {function}    onTypingDone           - function to call after user done typing
+ * @property {string}      [className]            - class name of input
+ */
 TypingDone.propTypes = {
   disabled: PropTypes.bool,
   readOnly: PropTypes.bool,
-  value: PropTypes.any,
-  defaultValue: PropTypes.any,
+  value: PropTypes.string,
+  defaultValue: PropTypes.string,
   placeholder: PropTypes.string,
   type: PropTypes.string,
   delay: PropTypes.number,
@@ -88,7 +178,20 @@ TypingDone.propTypes = {
   className: PropTypes.string,
 };
 
+/**
+ * @memberOf components.Globals.TypingDone.TypingDone
+ * @name defaultProps
+ * @type {object}
+ * @description defines default props of TypingDone
+ * @property {number}      delay          - default delay after user done typing
+ */
 TypingDone.defaultProps = {
   delay: 1000,
 };
+
+/**
+ * @memberOf components.Globals.TypingDone.TypingDone
+ * @export Places
+ * @description exports TypingDone module.
+ */
 export default TypingDone;
